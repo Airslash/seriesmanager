457af25a58548887f388b5fbd00694e46e335f5c
sofia86 <sofia@MacBook-Pro-de-Sofia.local>
2016-01-25 22:28:39 +0100

    css

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -16,7 +16,6 @@
 
 	<!--h1 class="hidden-xs">SeriesManager <?= $this->e($title) ?></h1--> 
 
-<<<<<<< HEAD
 
 	<!--Navigation bar -->
 
@@ -48,17 +47,10 @@
     		<!--Drop-down menu-->
 			<div class="collapse navbar-collapse" id="menu">
 		      	<ul class="nav navbar-nav">
-			        <li class="active"><a href="<?php echo $this->url('home') ?>" title="Accueil">Home</a></li>
-			        <li><a href="<?php echo $this->url('register') ?>" title="Inscription">Register</a></li>
+			        <li class="active"><a href="<?php echo $this->url('home') ?>" title="Home">Home</a></li>
+			        <li><a href="<?php echo $this->url('register') ?>" title="Signin">Register</a></li>
 			        <li><a href="<?php echo $this->url('profile') ?>" title="Profile">Profile</a></li>
-		       	</ul>
-=======
-			<nav>
-				<ul class="nav nav-tabs">
-					<li><a href="<?php echo $this->url('home') ?>" title="Home">Home</a></li>
-					<li><a href="<?php echo $this->url('register') ?>" title="Signin">Register</a></li>
-
-					<li><a href="<?php echo $this->url('profile') ?>" title="Profile">Profile</a></li>
+		       	</ul>		
 				
 				<form action="<?php echo $this->url('login') ?>" method="POST">
 					<input type="username" name="username" placeholder="Username">
@@ -67,15 +59,6 @@
 
 					<a href="<?php echo $this->url('password') ?>" title="Password">Password forgotten ?</a>
 				</form>
->>>>>>> origin/master
-
-
-      		<!--form class="navbar-form navbar-left" role="search">
-        	<div class="form-group">
-          		<input type="text" class="form-control" placeholder="Search">
-        	</div>
-        	<button type="submit" class="btn btn-default">Submit</button>
-      		</form-->
 
     		</div><!-- navbar-collapse -->
     		
@@ -94,19 +77,6 @@
 
 
 
-	<!--div class="container-fluid"
-
-				<button id="signIn" class="btn btn-default navbar-btn">Sign in</button>
-
-			</div>
-			</nav>
-
-					<input type="username" name="username" placeholder="Username">
-					<input type="password" name="password" placeholder="Password">
-					<button type="submit">Login</button>
-
-				
-		</header!-->
 
 		<section>
 			<?= $this->section('main_content') ?>
@@ -114,14 +84,16 @@
 
 		<footer>
 		</footer>
-	<!--/div-->
+
+
+
 
 	<script src="<?= $this->assetUrl('js/jquery-1.12.0.min.js') ?>"></script>
-<<<<<<< HEAD
+
 	<script src="<?= $this->assetUrl('js/bootstrap.min.js') ?>"></script>
-=======
+
 	<script src="<?= $this->assetUrl('js/masonry.pkgd.js') ?>"></script>
->>>>>>> origin/master
+
 	<script src="<?= $this->assetUrl('js/main.js') ?>"></script>
 
 </body>
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,13 @@
-<<<<<<< HEAD
+//Diaporama
   $(document).ready(function() {
     $('.carousel').carousel();
   });
-=======
+
+  
+
 $('.grid').masonry({
   // options
   itemSelector: '.grid-item',
   columnWidth: 150
 });
->>>>>>> origin/master
+
