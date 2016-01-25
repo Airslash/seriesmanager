07840753e4010539a1b8b49f80a1b4b129149daa
Airslash <merlin.axel@gmail.com>
2016-01-25 10:05:53 +0100

    modif detail

--- app/templates/serie/detail.php
+++ app/templates/serie/detail.php
@@ -7,10 +7,11 @@
 	
 		<div id="serie-info">
 			<h4><?= $serie["title"] ?></h4>
-			<img src="covers/<?= $serie["poster"] ?>" />
-			<div id="serie-description"><?= $serie["description"] ?></div>
+			<img src="../assets/img/<?= $serie["poster"] ?>" />
+			<div id="serie-start"><?= $serie["start_date"] ?></div>
 			<div id="serie-genre"><?= $serie["genre"] ?></div>
 			<div id="serie-actors"><?= $serie["actors"] ?></div>
+			<div id="serie-description"><?= $serie["description"] ?></div>
 		</div>
 	
 	<?php endforeach; ?>
