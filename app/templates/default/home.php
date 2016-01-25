a6612b94d349700f9806183b7f8cafc62a0a87a6
sofia86 <sofia@MacBook-Pro-de-Sofia.local>
2016-01-25 22:15:13 +0100

    CSS
    Ebauche css mobile

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -1,7 +1,31 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
+<?= $this->start('header') ?>
+
+<div class="container-fluid" id="header">
+
+	<div id="series-carousel" class="carousel container slide">
+  <div class="carousel-inner">
+            <div class="active item one"></div>
+            <div class="item two"></div>
+            <div class="item three"></div>
+            
+  </div>
+
+  </div>
+
+
+<?= $this->stop('header') ?>
+
 <?php $this->start('main_content') ?>
 	
 
-	
+
+
+
+
+
+
+ 
+ 
 <?php $this->stop('main_content') ?>
--- app/templates/layout.php
+++ app/templates/layout.php
@@ -2,33 +2,94 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title><?= $this->e($title) ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<title><?= $this->e($title) ?></title>
+	
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/normalize.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap.min.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
+
+
 <body>
-	<div class="container">
-		<header>
 
-			<h1>SeriesManager :: <?= $this->e($title) ?></h1>
+	<!--h1 class="hidden-xs">SeriesManager <?= $this->e($title) ?></h1--> 
 
-			<nav>
-				<ul class="nav nav-tabs">
-					<li><a href="<?php echo $this->url('home') ?>" title="Accueil">Accueil</a></li>
-					<li><a href="<?php echo $this->url('register') ?>" title="Inscription">Inscription</a></li>
+
+	<!--Navigation bar -->
+
+ 		<nav class="navbar navbar-default" role="navigation">
+ 		
+ 			<?= $this->section('header') ?> <!--Diaporama de la page home.php-->
+
+
+  		<div class="container-fluid">
+  			<div class="navbar-header">
+  				
+    	  			<!--burger-nav-->
+			     	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+			    	</button>
+
+			    	<!--logo-->
+    	  			<a class="navbar-brand" href="#home">SM</a>
+
+    	  			<button id="signIn" class="btn btn-default navbar-btn">Sign in</button>
+
+
+
+			</div><!-- navbar-header -->
+
+    		<!--Drop-down menu-->
+			<div class="collapse navbar-collapse" id="menu">
+		      	<ul class="nav navbar-nav">
+			        <li class="active"><a href="<?php echo $this->url('home') ?>" title="Accueil">Home</a></li>
+			        <li><a href="<?php echo $this->url('register') ?>" title="Inscription">Register</a></li>
+			        <li><a href="<?php echo $this->url('profile') ?>" title="Profile">Profile</a></li>
+		       	</ul>
+
+
+      		<!--form class="navbar-form navbar-left" role="search">
+        	<div class="form-group">
+          		<input type="text" class="form-control" placeholder="Search">
+        	</div>
+        	<button type="submit" class="btn btn-default">Submit</button>
+      		</form-->
+
+    		</div><!-- navbar-collapse -->
+    		
+		</div><!-- container-fluid-->
+		
+		</nav>
+
+
+		<form class="navbar-form" role="search">
+        	<div class="form-group">
+          		<input type="text" class="form-control" placeholder="Search">
+          	 </div>
+    			<button type="submit" class="btn btn-default">Submit</button>
+      	</form>
+
+
+
+
+	<!--div class="container-fluid"
+
+				<button id="signIn" class="btn btn-default navbar-btn">Sign in</button>
+
+			</div>
+			</nav>
 
 					<input type="username" name="username" placeholder="Username">
 					<input type="password" name="password" placeholder="Password">
 					<button type="submit">Login</button>
 
-					<li><a href="<?php echo $this->url('profile') ?>" title="Profile">Profile</a></li>
-
-				</ul>
-			</nav>
-
-			
-		</header>
+				
+		</header!-->
 
 		<section>
 			<?= $this->section('main_content') ?>
@@ -36,9 +97,11 @@
 
 		<footer>
 		</footer>
-	</div>
+	<!--/div-->
 
 	<script src="<?= $this->assetUrl('js/jquery-1.12.0.min.js') ?>"></script>
+	<script src="<?= $this->assetUrl('js/bootstrap.min.js') ?>"></script>
+	<script src="<?= $this->assetUrl('js/main.js') ?>"></script>
 
 </body>
 </html>
