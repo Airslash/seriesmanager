22c222d7bbc5931ac82e485e9ffdffc2b656e686
Airslash <merlin.axel@gmail.com>
2016-01-29 15:11:12 +0100

    ajax

--- app/templates/serie/search.php
+++ app/templates/serie/search.php
@@ -1,7 +1,9 @@
 <?php foreach ($series as $serie): ?>
 
 	<div id="serie-info">
-		<a href="detail/<?= $serie['id'] ?>/"><img src="http://ia.media-imdb.com/images/M/<?= $serie["poster_id"] ?>._V1._SY74_CR0,0,54,74_.jpg" /><?= $serie["title"] ?> (<?= $serie["start_date"] ?>)</a>
+		<a href="detail/<?= $serie['id'] ?>/">
+			<img src="http://ia.media-imdb.com/images/M/<?= $serie["poster_id"] ?>._V1_UX67_CR0,0,67,98_AL_.jpg"  /><?= $serie["title"] ?> (<?= $serie["start_date"] ?>)
+		</a>
 	</div>
 
 <?php endforeach; ?>
