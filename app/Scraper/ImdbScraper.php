3baaa0a1fb812c55b1d688547746272b2f780b78
TangoMan75 <matthias.morin@gmail.com>
2016-02-01 16:28:44 +0100

    Class ImdbScraper documentation

--- app/Scraper/ImdbScraper.php
+++ app/Scraper/ImdbScraper.php
@@ -8,10 +8,15 @@ namespace Scraper;
  * Scrapes user query from imdb and returns all scraped data
  * Seasons and episodes arrays are indexed starting from 1
  * 
- * @version        2.5.1
- * @last_modified  20:19 31/01/2016
+ * @version        2.5.2
+ * @last_modified  16:25 01/02/2016
  * @author         Matthias Morin <matthias.morin@gmail.com>
  * @copyright      2015-2016 - CAMS Squad, Full Stack Web Developpers Team
+	* @method         __construct           Initializes $context propery
+	* @method         scrapeSeriesId        Builds $imdb_id list from imdb result page url (50 elements each)
+	* @method         scrapeSerieById       Builds imdb url from user query and scrapes imdb serie details
+	* @method         scrapeSeasons         Builds imdb season url from $imdb_id, $season_count and sends each episode found to scrapeEpisodeDetails()
+	* @method         scrapeEpisodeDetails  Scrapes epidode details from imdb
  */
 Class ImdbScraper {
 	/**
@@ -212,7 +217,7 @@ Class ImdbScraper {
 	/**
 	 * scrapeEpisodeDetails
 	 * 
-	 * Scrapes imdb episodes details
+	 * Scrapes epidode details from imdb
 	 * 
 	 * @version                       2.4
 	 * @param   integer     $season   Season
