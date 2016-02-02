3e737179bb24f1caf261b37657bc82bfef5874b3
Airslash <merlin.axel@gmail.com>
2016-02-02 10:50:22 +0100

    essai collection

--- app/Manager/UserManager.php
+++ app/Manager/UserManager.php
@@ -6,5 +6,10 @@ namespace Manager;
 
 class UserManager extends \W\Manager\UserManager
 {
+	public function collection() {
 
+		$sql = "INSERT INTO bookmarks (user_id, serie_id) VALUES ($w_user);";
+		$statement = $pdo->prepare($sql);
+		$statement->execute([$_GET["id"]]);
+	}
 }
